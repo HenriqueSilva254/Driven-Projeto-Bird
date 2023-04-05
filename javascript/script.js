5291c0827ceb3a99daba9562f820593cf6ca1bee
HenriqueSilva254 <henriquesilva254@gmail.com>
2023-04-05 18:21:09 -0300

    Parrot game -  Dar as cartas ok

--- javascript/script.js
+++ javascript/script.js
@@ -6,4 +6,14 @@ function iniciarJogo (){
     while(numeroCartas % 2 !== 0 || numeroCartas < 4 || numeroCartas > 14){
         numeroCartas = parseInt(prompt("O numero de cartas tem que ser um n° par entre 4 e 14"))
     }
+    const div = document.querySelector('.cartas')
+    let contador = 0;
+    while(contador < numeroCartas){
+        div.innerHTML += "<div class='card'><div class='front-face face'></div><div class='back-face face'><img class='cartaVirada' src='./imagens/metalparrot.gif' alt=''></div></div>"
+        
+        contador++;
+        console.log()    
+    }
+    
 }
+
