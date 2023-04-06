d92230b740cac1cb5bc417767d5e952a04517871
HenriqueSilva254 <henriquesilva254@gmail.com>
2023-04-06 20:46:31 -0300

    Parrot game - Cartas Embaralhadas

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,20 @@
+
+
+const imagensCartas = [
+'bobrossparrot',
+'explodyparrot',
+'fiestaparrot',
+'metalparrot',
+'revertitparrot',
+'tripletsparrot',
+'unicornparrot']
+
+
+const cartas = [];
+
+
+
+
 iniciarJogo();
 
 function iniciarJogo (){
@@ -8,12 +25,32 @@ function iniciarJogo (){
     }
     const div = document.querySelector('.cartas')
     let contador = 0;
+    const width = document.querySelector('.cartas')
+    width.style.width = 73 * numeroCartas + 'px'
+    let parCartas = 0
+    let contadorimg = 0
     while(contador < numeroCartas){
-        div.innerHTML += "<div class='card' data-test='card'><div class='front-face face'><img data-test='face-down-image' src='../imagens/back.png' alt=''></div><div class='back-face face'><img data-test='face-up-image' class='cartaVirada' src='./imagens/metalparrot.gif' alt=''></div></div>"
-        
-        contador++;
-        console.log()    
+        cartas.push(`data-test='card'><div class='front-face face'><img data-test='face-down-image' src='../imagens/back.png' alt=''></div><div class='back-face face'><img data-test='face-up-image' class='cartaVirada' src='./imagens/${imagensCartas[contadorimg]}.gif' alt=''></div>`)
+        cartas.push(`data-test='card'><div class='front-face face'><img data-test='face-down-image' src='../imagens/back.png' alt=''></div><div class='back-face face'><img data-test='face-up-image' class='cartaVirada' src='./imagens/${imagensCartas[contadorimg]}.gif' alt=''></div>`)
+        contador = contador + 2;
+        contadorimg++;
+        console.log(contador)
     }
     
+    function comparador() { 
+        return Math.random() - 0.5; 
+    }
+
+    cartas.sort(comparador);
+
+    
+    for(let i = 0; i <cartas.length; i++){
+        
+        div.innerHTML += `<div class='card'${cartas[i]}</div>`
+
+    }
 }
+
+ 
+
 
